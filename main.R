ba91d001fb5f2251a0c470ec4139c88c02816951
Dimitrios Loukas <99596951+LoukasDimitrios@users.noreply.github.com>
2024-05-20 16:04:51 +0200

    Update main.R

--- main.R
+++ main.R
@@ -1,5 +1,5 @@
 #Libraries
-source('Libraries')
+source('Libraries.R')
 
 #Load data
 load("data_count.Rdata")
@@ -30,20 +30,26 @@ source('Populations.R')
 #Subpopulations
 source('Subpopulations.R')
 
-#GridSearch Unscaled
-#GridSearch Scaled - groups
-#GridSearch Scaled - LFC
+#Perform GridSearch on the Unscaled data
+source('GridSearch_unscaled.R')
+
+#Perform GridSearch on the Scaled data with groups defined by expert immunologists
+source('GridSearch_scaled.R')
+
+#Perform GridSearch on the Scaled data with groups based on Log Fold Change
+source('Log-Fold-Change_groups.R')
+source('GridSearch_scaled_lfc.R')
 
 
 # Perform Clustering on a bigger subset
-# COUNTS
-pops <- expr_count$population
-subpops <- expr_count$subpopulation
-lineage_channels <- c("CD57", "CD19", "CD4", "CD8", "IgD", "CD11c", "CD16", "CD3", "CD38", "CD27", "CD14", "CXCR5", "CCR7", "CD45RA", "CD20", "CD127", "CD33", "CD28", "CD161", "TCRgd", "CD123", "CD56", "HLADR", "CD25")
-expr <- expr_count[,lineage_channels]
 
+# Load marker scaling groups defined by experts immunologists
+source('Groups.R')
 
-# Subset data
+# Load Normalization chunk for marker scaling groups
+source('Normalization.R')
+
+# Create a bigger Subset data
 set.seed(42)
 subset <- sample(nrow(expr), 500000)
 expr_sub <- expr[subset,]
@@ -51,4 +57,9 @@ pops_sub <- pops[subset]
 subpops_sub <- subpops[subset]
 
 #Seurat
+source('Seurat_unscaled_clustering')
+source('Seurat_scaled_clustering')
+
 #Phenograph
+source('Phenograph_clustering_unscaled.R')
+source('Phenograph_clustering_scaled.R')
