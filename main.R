6f312a461b72010b801eaf47e06b1933f0377f59
Dimitrios Loukas <99596951+LoukasDimitrios@users.noreply.github.com>
2024-05-20 15:04:21 +0200

    Update main.R

--- main.R
+++ main.R
@@ -8,3 +8,21 @@
 #GridSearch Scaled - groups
 #GridSearch Scaled - LFC
 
+
+# Perform Clustering on a bigger subset
+# COUNTS
+pops <- expr_count$population
+subpops <- expr_count$subpopulation
+lineage_channels <- c("CD57", "CD19", "CD4", "CD8", "IgD", "CD11c", "CD16", "CD3", "CD38", "CD27", "CD14", "CXCR5", "CCR7", "CD45RA", "CD20", "CD127", "CD33", "CD28", "CD161", "TCRgd", "CD123", "CD56", "HLADR", "CD25")
+expr <- expr_count[,lineage_channels]
+
+
+# Subset data
+set.seed(42)
+subset <- sample(nrow(expr), 500000)
+expr_sub <- expr[subset,]
+pops_sub <- pops[subset]
+subpops_sub <- subpops[subset]
+
+#Seurat
+#Phenograph
